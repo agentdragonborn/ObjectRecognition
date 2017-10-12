2439974e0625824171ee3867a8b373465b7e47ae
Shivam Sardana <shivamsardana@bitbucket.org>
2017-10-12 23:35:00 +0530

    First Commit

--- vehicle_detection.py
+++ vehicle_detection.py
@@ -47,3 +47,18 @@ model.add(Dense(1470))
 
 
 model.summary()
+load_weights(model,'./yolo-tiny.weights')
+
+imagePath = './test_images/test1.jpg'
+image = plt.imread(imagePath)
+image_crop = image[300:650,500:,:]
+resized = cv2.resize(image_crop,(448,448))
+batch = np.transpose(resized,(2,0,1))
+batch = 2*(batch/255.) - 1
+batch = np.expand_dims(batch, axis=0)
+out = model.predict(batch)
+boxes = yolo_net_out_to_car_boxes(out[0], threshold = 0.17)
+f,(ax1,ax2) = plt.subplots(1,2,figsize=(16,6))
+ax1.imshow(image)
+ax2.imshow(draw_box(boxes,plt.imread(imagePath),[[500,1280],[300,650]]))
+plt.show()
